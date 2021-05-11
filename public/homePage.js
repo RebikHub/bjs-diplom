ae69a28081ed56bedf830fdb42a78161371b4b47
Alexandr Rebikov <rebikovlk@mail.ru>
2021-05-11 12:32:14 +0700

    addMoney

--- public/homePage.js
+++ public/homePage.js
@@ -1,12 +1,10 @@
 'use strict';
 
-// const getStocks = require("../routes/stocks");
-
 const logoutBtn = new LogoutButton;
+const currentRates = new RatesBoard;
+const userMoney = new MoneyManager;
 
-logoutBtn.action = () => {
-    return ApiConnector.logout(() => location.reload());
-};
+logoutBtn.action = () => ApiConnector.logout(() => location.reload());
 
 ApiConnector.current(response => {
     if (response.success === true) {
@@ -15,6 +13,20 @@ ApiConnector.current(response => {
     return alert(response.error);
 });
 
-const currentRates = new RatesBoard;
+ApiConnector.getStocks(data => {
+    if (data.success === false) {
+        return data.error;
+    };
+    return currentRates.fillTable(data.data);
+});
 
-currentRates.fillTable(ApiConnector.getStocks(response => console.log(response.success)))
+userMoney.addMoneyCallback = (data) => {
+    return ApiConnector.addMoney(data, (response) => {
+        if (response.success === false) {
+            return userMoney.setMessage(response.success, response.error);
+        };
+        ProfileWidget.showProfile(response.data);
+        return userMoney.setMessage(response.success, 'ok!'); 
+    });
+};
+
