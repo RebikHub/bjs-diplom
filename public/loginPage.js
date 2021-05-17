3d6a475cba4cefb680639f3b4151af03eafd8c57
Alexandr Rebikov <rebikovlk@mail.ru>
2021-05-17 09:56:17 +0700

    Error login and register

--- public/loginPage.js
+++ public/loginPage.js
@@ -5,7 +5,7 @@ const userForm = new UserForm;
 userForm.loginFormCallback = (data) => {
     return ApiConnector.login(data, (response) => {
         if (response.success === false) {
-            return alert(response.error);
+            return userForm.setLoginErrorMessage(response.error);
         }
         return location.reload();
     });
@@ -14,7 +14,7 @@ userForm.loginFormCallback = (data) => {
 userForm.registerFormCallback = (data) => {
     return ApiConnector.register(data, (response) => {
         if (response.success === false) {
-            return alert(response.error);
+            return userForm.setRegisterErrorMessage(response.error);
         }
         return location.reload();
     });
